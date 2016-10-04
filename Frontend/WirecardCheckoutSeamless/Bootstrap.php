ea96594ac44c6cb4c57a229eaf34d3bdb587b048
Jacqueline Rinnhofer <jacqueline.rinnhofer@partner.wirecard.com>
2016-10-04 14:12:20 +0200

    updated pluginversion

--- Frontend/WirecardCheckoutSeamless/Bootstrap.php
+++ Frontend/WirecardCheckoutSeamless/Bootstrap.php
@@ -64,7 +64,7 @@ class Shopware_Plugins_Frontend_WirecardCheckoutSeamless_Bootstrap extends Shopw
      */
     public function getVersion()
     {
-        return '1.7.23';
+        return '1.7.24';
     }
 
     /**
