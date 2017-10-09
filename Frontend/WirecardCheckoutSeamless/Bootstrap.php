3e1892c66ce04ac8f7b2f78a1587c7d2953c4e15
jakubpolomsky <jakub.polomsky@partner.wirecard.com>
2017-10-09 14:43:38 +0200

    #66 reserve the items in the db

--- Frontend/WirecardCheckoutSeamless/Bootstrap.php
+++ Frontend/WirecardCheckoutSeamless/Bootstrap.php
@@ -64,7 +64,7 @@ class Shopware_Plugins_Frontend_WirecardCheckoutSeamless_Bootstrap extends Shopw
      */
     public function getVersion()
     {
-        return '1.10.7';
+        return '1.10.8';
     }
 
     /**
