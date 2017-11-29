20b09bfc7dcc788f182abb07b6912d0054887124
tomazpu <32056440+tomazpu@users.noreply.github.com>
2017-11-29 13:49:10 +0100

    #69 version increase

--- Frontend/WirecardCheckoutSeamless/Bootstrap.php
+++ Frontend/WirecardCheckoutSeamless/Bootstrap.php
@@ -64,7 +64,7 @@ class Shopware_Plugins_Frontend_WirecardCheckoutSeamless_Bootstrap extends Shopw
      */
     public function getVersion()
     {
-        return '1.10.8';
+        return '1.10.9';
     }
 
     /**
